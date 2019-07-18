0d71acf4813319a60904b1ff652808e4dfcb6c8d
Phillip Moreno <phillipmoreno123@gmail.com>
2019-07-18 00:38:18 -0400

    Added animations to buttons

--- Mad Libs Game/src/MadLibsMainMenuController.java
+++ Mad Libs Game/src/MadLibsMainMenuController.java
@@ -1,13 +1,15 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
-public class MadLibsMainMenuController {
-
-    @FXML
-    private AnchorPane circle1;
+public class MadLibsMainMenuController implements Initializable {
 
     @FXML
     private Button story1;
@@ -17,9 +19,6 @@ public class MadLibsMainMenuController {
 
     @FXML
     private Button story3;
-
-    @FXML
-    private Circle circle2;
 
     @FXML
     void handleButtonAction(ActionEvent event) {
@@ -32,4 +31,30 @@ public class MadLibsMainMenuController {
     	}
     }
 
+    public void initialize(URL url, ResourceBundle rb) {
+    	Circle circle = new Circle(9);
+    	PathTransition transition1 = new PathTransition();
+    	PathTransition transition2 = new PathTransition();
+    	PathTransition transition3 = new PathTransition();
+    	
+    	transition1.setNode(story1);
+    	transition2.setNode(story2);
+    	transition3.setNode(story3);
+    	
+    	transition1.setDuration(Duration.seconds(2));
+    	transition2.setDuration(Duration.seconds(2));
+    	transition3.setDuration(Duration.seconds(2));
+
+    	transition1.setPath(circle);
+    	transition2.setPath(circle);
+    	transition3.setPath(circle);
+    	
+    	transition1.setCycleCount(PathTransition.INDEFINITE);
+    	transition2.setCycleCount(PathTransition.INDEFINITE);
+    	transition3.setCycleCount(PathTransition.INDEFINITE);
+    	
+    	transition1.play();
+    	transition2.play();
+    	transition3.play();
+    }
 }
