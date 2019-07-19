4c21ed3b12001c7bfad0dbf72abe3e33a2407fdf
Phillip Moreno <phillipmoreno123@gmail.com>
2019-07-18 21:43:25 -0400

    GUI created for Story 2

---
+++ Mad Libs Game/src/MadLibsOutput2Controller.java
@@ -0,0 +1,47 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MadLibsOutput2Controller {
+
+    @FXML
+    private TextField nameTF;
+
+    @FXML
+    private TextField adjectiveTF1;
+
+    @FXML
+    private TextField nounTF;
+
+    @FXML
+    private TextField expressionTF;
+
+    @FXML
+    private TextField numberTF;
+
+    @FXML
+    private TextField adjectiveTF2;
+
+    @FXML
+    private TextField schoolTF;
+
+    @FXML
+    private TextField restaurantTF;
+
+    @FXML
+    private TextField foodTF;
+
+    @FXML
+    private TextField liquidTF;
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openNewStage(ActionEvent event) {
+
+    }
+
+}
