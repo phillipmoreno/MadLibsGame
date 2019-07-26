1b458b80bce7df6159782c61de679c0cb97dabd7
Phillip Moreno <phillipmoreno123@gmail.com>
2019-07-26 16:26:14 -0400

    Updated Main Menu GUI

--- Mad Libs Game/src/MadLibsMainMenuController.java
+++ Mad Libs Game/src/MadLibsMainMenuController.java
@@ -31,15 +31,15 @@ public class MadLibsMainMenuController implements Initializable {
 
 	@FXML
 	private Button story3;
-	
+
 	@FXML
-    private Polygon triangle1;
+	private Polygon triangle1;
 
-    @FXML
-    private Polygon triangle2;
+	@FXML
+	private Polygon triangle2;
 
-    @FXML
-    private Polygon triangle3;
+	@FXML
+	private Polygon triangle3;
 
 	@FXML
 	private Button exit;
@@ -63,7 +63,7 @@ public class MadLibsMainMenuController implements Initializable {
 	PathTransition transition1 = new PathTransition();
 	PathTransition transition2 = new PathTransition();
 	PathTransition transition3 = new PathTransition();
-	
+
 	RotateTransition transition4 = new RotateTransition(Duration.seconds(1.2), triangle1);
 	RotateTransition transition5 = new RotateTransition(Duration.seconds(1.2), triangle2);
 	RotateTransition transition6 = new RotateTransition(Duration.seconds(1.2), triangle3);
@@ -71,7 +71,7 @@ public class MadLibsMainMenuController implements Initializable {
 	@FXML
 	void handleButtonAction(ActionEvent event) {
 		Stage menu = (Stage) exit.getScene().getWindow();
-    	menu.close();
+		menu.close();
 		if (event.getSource() == story1) {
 			try {
 				// FXMLLoader object is created to load in fxml file
@@ -147,25 +147,31 @@ public class MadLibsMainMenuController implements Initializable {
 			story1.setEffect(shadow1);
 			transition1.stop();
 			transition4.stop();
+			triangle1.setFill(Color.LIGHTGREEN);
 		} else if (event.getSource() == story2) {
 			story2.setEffect(shadow2);
 			transition2.stop();
 			transition5.stop();
+			triangle2.setFill(Color.LIGHTGREEN);
 		} else if (event.getSource() == story3) {
 			story3.setEffect(shadow3);
 			transition3.stop();
 			transition6.stop();
+			triangle3.setFill(Color.LIGHTGREEN);
 		}
 	}
 
 	@FXML
 	void play(MouseEvent event) {
+		triangle1.setFill(Color.DODGERBLUE);
 		transition1.play();
 		transition4.play();
 		story1.setEffect(null);
+		triangle2.setFill(Color.RED);
 		transition2.play();
 		transition5.play();
 		story2.setEffect(null);
+		triangle3.setFill(Color.GOLD);
 		transition3.play();
 		transition6.play();
 		story3.setEffect(null);
@@ -180,33 +186,29 @@ public class MadLibsMainMenuController implements Initializable {
 		transition4.setNode(triangle1);
 		transition5.setNode(triangle2);
 		transition6.setNode(triangle3);
-		
-		
+
 		transition4.setByAngle(360);
 		transition5.setByAngle(360);
 		transition6.setByAngle(360);
-		
+
 		transition4.setAutoReverse(false);
 		transition5.setAutoReverse(false);
 		transition6.setAutoReverse(false);
-	
+
 		transition1.setDuration(Duration.seconds(1.2));
 		transition2.setDuration(Duration.seconds(1.2));
 		transition3.setDuration(Duration.seconds(1.2));
-	
 
 		transition1.setPath(rectangle);
 		transition2.setPath(circle);
 		transition3.setPath(rectangle);
-	
-		
+
 		transition1.setCycleCount(PathTransition.INDEFINITE);
 		transition2.setCycleCount(PathTransition.INDEFINITE);
 		transition3.setCycleCount(PathTransition.INDEFINITE);
 		transition4.setCycleCount(RotateTransition.INDEFINITE);
 		transition5.setCycleCount(RotateTransition.INDEFINITE);
 		transition6.setCycleCount(RotateTransition.INDEFINITE);
-		
 
 		transition1.play();
 		transition2.play();
@@ -214,7 +216,6 @@ public class MadLibsMainMenuController implements Initializable {
 		transition4.play();
 		transition5.play();
 		transition6.play();
-		
 
 	}
 }
